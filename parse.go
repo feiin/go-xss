168f5d617d23106a809bfa4d87e3a08f7ea0610e
solar <feiin@qq.com>
2021-01-05 16:35:47 +0800

    added parseAttr

--- parse.go
+++ parse.go
@@ -2,7 +2,16 @@ package xss
 
 import (
 	"strings"
+	"regexp"
+	// "fmt"
 )
+
+type OnTagFunc func(sourcePosition int,position int,tag string,html string, isClosing bool) string
+type escapeFunc func(html string) string
+type onAttrFunc func(name, value string) string
+
+var regAttr = regexp.MustCompile("[^a-zA-Z0-9_:\\.\\-]")
+var regEmtpy = regexp.MustCompile("\\s|\\n|\\t")
 
 //getTagName get tag name
 func getTagName(html string) string {
@@ -28,6 +37,195 @@ func getTagName(html string) string {
 	return tagName
 }	
 
+//parseTag
+func parseTag(html string,onTag OnTagFunc,escapeHtml escapeFunc) string {
+
+	rethtml := ""
+	lastPos := 0
+	tagStart := -1
+	quoteStart := ""
+	currentPos := 0
+	htmlLen := len(html)
+	currentTagName := ""
+	currentHtml := ""
+
+	chariterator:
+		for currentPos = 0;currentPos<htmlLen;currentPos++ {
+			c := html[currentPos:currentPos+1]
+
+			if tagStart == -1 {
+				if c == "<" {
+					tagStart = currentPos
+					continue
+				}
+			} else {
+				if quoteStart == "" {
+
+					if (c == "<") {
+						rethtml += escapeHtml(html[lastPos:currentPos])
+						tagStart = currentPos 
+						lastPos = currentPos
+						continue
+					}
+
+					if c == ">" {
+						rethtml += escapeHtml(html[lastPos:tagStart])
+						currentHtml = html[tagStart:currentPos+1]
+						currentTagName = getTagName(currentHtml)
+
+						rethtml += onTag(tagStart, len(rethtml), currentTagName, currentHtml, isClosing(currentHtml))
+
+						lastPos = currentPos + 1
+						tagStart = -1
+						continue
+
+					}
+					
+					if c == "'" || c == "\"" {
+
+						i := 1
+						ic := html[currentPos-i:currentPos]
+
+						for  {
+
+							if !(ic == " " || ic == "=") {
+								break;
+							}
+
+							if ic == "=" {
+								quoteStart = c
+								continue chariterator
+							}
+							i = i+1
+							ic = html[currentPos-i:currentPos-i+1]
+						}
+
+					}
+
+				} else {
+					if c == quoteStart {
+						quoteStart = ""
+						continue
+					}	
+				}	
+			}
+		}
+
+		if lastPos < len(html) {
+			rethtml += escapeHtml(html[lastPos:])
+		}
+
+		return rethtml
+
+}
+
+
+//parseAttr
+func parseAttr(html string,onAttr onAttrFunc) string {
+
+	lastPos := 0
+	retAttr :=  []string{}
+	tmpName := ""
+	htmlLen := len(html)
+
+	addAttr := func(name string, value string) {
+		name = strings.TrimSpace(name)
+		name =  strings.ToLower(regAttr.ReplaceAllString(name, ""))
+		if len(name) < 1 {
+			return
+		}
+	
+		ret := onAttr(name, value)
+		if len(ret) > 0 {
+			retAttr = append(retAttr,ret)
+		}
+	
+	}
+
+
+	for i := 0; i < htmlLen; i++ {
+		c := html[i:i+1]
+
+		v := ""
+		j := -1
+
+ 		if tmpName == "" && c == "=" {
+			tmpName = html[lastPos:i]
+			lastPos = i + 1
+			continue
+		}
+
+		
+		if tmpName != "" {
+
+			if i==lastPos && (c == "\""  || c == "'") && html[i-1:i] == "=" {
+				j = strings.Index(html[i+1:],c)
+				j = i+1+j
+				if j == -1 {
+					break
+				} else {
+ 
+					v = strings.TrimSpace(html[lastPos+1:j])
+					addAttr(tmpName,v)
+					tmpName = ""
+					i = j
+					lastPos = i + 1
+					continue
+
+				}
+
+			}
+		}
+
+		//empty
+		if regEmtpy.MatchString(c) {
+ 
+			html = regEmtpy.ReplaceAllString(html," ")
+ 
+			if tmpName == "" {
+				j = findNextEqual(html,i)
+ 
+				if j == -1 {
+					v = strings.TrimSpace(html[lastPos:i])
+					addAttr(v,"")
+					tmpName = ""
+					lastPos = i + 1
+					continue
+				} else {
+					i = j - 1 
+					continue
+				}
+			} else {
+				
+				j = findBeforeEqual(html,i-1)
+		
+
+				if j == -1 {
+					v = strings.TrimSpace(html[lastPos:i]);
+					v = stripQuoteWrap(v);
+					addAttr(tmpName, v);
+					tmpName = "";
+					lastPos = i + 1;
+					continue;
+				} else {
+					continue
+				}
+		
+			}
+		}
+	}
+
+	if lastPos < len(html) {
+
+		if tmpName == "" {
+			addAttr(html[lastPos:],"")
+		} else {
+			addAttr(tmpName, stripQuoteWrap(strings.TrimSpace(html[lastPos:])));
+		}
+	}
+
+	return strings.TrimSpace(strings.Join(retAttr, " "))
+}
 
 //isClosing is close tag
 func isClosing(html string) bool{
@@ -55,4 +253,42 @@ func stripQuoteWrap(text string) string {
 		return text[1:len(text)-1]
 	}
 	return text
+}
+
+
+func findNextEqual(str string,i int) int {
+
+
+	for ix:=i;ix<len(str);ix++ {
+		c := str[ix:ix+1]
+		if  c == " " {
+			continue
+		} 
+		if c == "=" {
+			return ix
+		}
+		return -1;
+
+	}
+	return -1;
+
+}
+
+func findBeforeEqual(str string,i int) int {
+
+	for ix:=i;ix>0;ix-- {
+
+		c := str[ix:ix+1]
+		if  c == " " {
+			continue
+		} 
+		if c == "=" {
+			return ix
+		}
+		return -1;
+
+	}
+	return -1;
+
+
 }
--- parse_test.go
+++ parse_test.go
@@ -2,7 +2,7 @@ package xss
 
 import (
 	"testing"
-	// "fmt"
+	"fmt"
 
 )
 
@@ -81,3 +81,124 @@ func TestStripQuoteWrap(t *testing.T) {
 	}
 }
 
+
+
+
+
+
+func escapeHtml(input string) string {
+	return input
+}
+
+func TestParseTag(t *testing.T) {
+	xh := "hello<A href=\"#\">www</A>ccc<b><br/>"
+
+	index := 0
+	onTag := func(sourcePosition int,position int,tag string,html string, isClosing bool) string {
+
+		// fmt.Printf("...onTag  tagname:%s\n  html:%s",tag,html)
+
+		if index == 0 {
+			if tag == "a" && html == "<A href=\"#\">" {
+				t.Logf("parse tag success tag:%s html:%s",tag, html)
+			} else {
+				t.Errorf("parse tag failed")
+			}
+		}
+
+		if index == 1 {
+			if tag == "a" && html == "</A>" {
+				t.Logf("parse tag success tag:%s html:%s",tag, html)
+			} else {
+				t.Errorf("parse tag failed")
+			}
+		}
+
+		if index == 2 {
+			if tag == "b" && html == "<b>" {
+				t.Logf("parse tag success tag:%s html:%s",tag, html)
+			} else {
+				t.Errorf("parse tag failed")
+			}
+		}
+
+		if index == 3 {
+			if tag == "br" && html == "<br/>" {
+				t.Logf("parse tag success tag:%s html:%s",tag, html)
+			} else {
+				t.Errorf("parse tag failed")
+			}
+		}
+	
+		index++
+		return html
+	}
+
+	result := parseTag(xh,onTag,escapeHtml)
+	
+	if result != xh {
+		t.Errorf("parseTag err  %v",result)
+		return
+	} 
+	t.Logf("parseTag %v",result)
+}
+
+func TestParseAttr(t *testing.T) {
+	xh := "href=\"#\"attr1=b attr2=c attr3 attr4='value4\"'attr5"
+
+	index := 0
+
+	onAttr := func (name string ,value string) string {
+
+		fmt.Printf("name:%s,value:%s\n",name,value)
+		if index == 0 {
+			if name == "href" && value == "#" {
+				t.Logf("parse attr success name:%s value:%s",name, value)
+			} else {
+				t.Errorf("parse attr failed")
+			}
+		}
+		if index == 1 {
+			if name == "attr1" && value == "b" {
+				t.Logf("parse attr success name:%s value:%s",name, value)
+			} else {
+				t.Errorf("parse attr failed")
+			}
+		}
+		if index == 2 {
+			if name == "attr2" && value == "c" {
+				t.Logf("parse attr success name:%s value:%s",name, value)
+			} else {
+				t.Errorf("parse attr failed")
+			}
+		}
+		if index == 3 {
+			if name == "attr3" && value == "" {
+				t.Logf("parse attr success name:%s value:%s",name, value)
+			} else {
+				t.Errorf("parse attr failed")
+			}
+		}
+		if index == 4 {
+			if name == "attr4" && value == "value4\"" {
+				t.Logf("parse attr success name:%s value:%s",name, value)
+			} else {
+				t.Errorf("parse attr failed")
+			}
+		}
+		if index == 5 {
+			if name == "attr5" && value == "" {
+				t.Logf("parse attr success name:%s value:%s",name, value)
+			} else {
+				t.Errorf("parse attr failed")
+			}
+		}
+		index++
+		return name + "=" + value
+	}
+
+	result := parseAttr(xh,onAttr)
+	
+	 
+	t.Logf("parseAttr result %v",result)
+}
